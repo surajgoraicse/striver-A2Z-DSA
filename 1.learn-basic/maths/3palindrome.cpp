86126a989645e6ca077f57e9d9679294c00c8a0c
surajgoraicse <surajgoraicse@gmail.com>
2024-10-14 00:28:58 +0530

    improvement in the code

--- 1.learn-basic/maths/3palindrome.cpp
+++ 1.learn-basic/maths/3palindrome.cpp
@@ -1,25 +1,17 @@
 #include <iostream>
 using namespace std;
 bool palindrome(int);
-bool palindrome(int num)
+bool palindrome(int x)
 {
-    int temp = num;
-    int digit, rev = 0;
-
-    while (temp)
-    {
-        digit = temp % 10;
-        rev = rev * 10 + digit;
+      if(x < 0) return 0;
+     int temp = x;
+     long long int rev = 0;
+     while(temp){
+        rev = rev * 10 + temp % 10;
         temp /= 10;
-    }
-    if (num == rev)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+     }
+     bool ans = rev == x;
+     return ans;
 }
 int main()
 {
